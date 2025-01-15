17ec0c31af99c4a6df4e5a88ca1a4ee418c3382a
uix-amit <amit.uiexplorer@gmail.com>
2025-01-15 16:45:19 +0530

    fix(card): authorization header required for accessing cards API
    jwt auth guard added to the cards API path, so that all calls to cards path should provide the
    Authorization header mandatorily.

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -6,10 +6,12 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiHeader, ApiTags } from '@nestjs/swagger';
 import { Cards } from '@prisma/client';
 
+import { JwtAuthGuard } from '@auth/jwt-auth/jwt-auth.guard';
 import { CardsService } from '@cards/cards.service';
 import { CreateCardDto } from '@cards/dto/create-card-dto';
 import { UpdateCardDto } from '@cards/dto/update-card-dto';
@@ -22,6 +24,7 @@ import { IdValidationDto } from '@shared/validators/id-validation-dto';
   description: 'Bearer token',
   required: true,
 })
+@UseGuards(JwtAuthGuard)
 @Controller({ path: 'cards', version: '1' })
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
